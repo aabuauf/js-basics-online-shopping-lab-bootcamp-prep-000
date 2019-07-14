cd4069f94496f4aa0264ffb0854abdd8d20a25dd
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 20:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === ""){
+  if (cardNumber === undefined ){
     return "Sorry, we don't have a credit card on file for you."
   }else{
     cart =[]
