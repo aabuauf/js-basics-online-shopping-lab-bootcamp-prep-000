40ad52558afcb7e7c4ec529f892c3dfdf9a85489
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 19:45:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length<1){return "Your shopping cart is empty."}
   for (var i= 0; i< cart.length;i++){
-    if (cart.length<1){return "Your shopping cart is empty."}
+    
    itemName = cart[i].itemName
    itemPrice = cart[i].itemPrice
     name = " " + itemName + " at $" + itemPrice + ","
