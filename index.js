c5b69cfac099cb0c756a9e21c52ffc527b14754c
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 20:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }else{
     cart =[]
-    return "Your total cost is $"+ total()  +", which will be charged to the card 83296759."
+    return "Your total cost is $"+ total()  +", which will be charged to the card "+ cardNumber +"."
   }
 }
